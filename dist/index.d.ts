0b83686297d0b1e779ff5215d8fb3ec07d0d1df1
prostarz <tommyspam2000@gmail.com>
2024-09-12 12:27:13 +0100

    (feat): build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,6 @@
 export * from "./@types";
 export * from "./classes";
+import * as cheerio from "cheerio";
+import * as fuse from "fuse.js";
+export { cheerio, fuse };
 //# sourceMappingURL=index.d.ts.map
